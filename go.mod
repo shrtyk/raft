9e84f23624256b3e4e07db42811d0a76be21a683
shortyk <shortykofficial@gmail.com>
2025-09-16 00:33:43 +0300

    feat: initial setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shrtyk/raft
+
+go 1.25.0
+
+require github.com/anishathalye/porcupine v1.0.3
---
+++ shardkv1/client.go
@@ -0,0 +1,51 @@
+package shardkv
+
+//
+// client code to talk to a sharded key/value service.
+//
+// the client uses the shardctrler to query for the current
+// configuration and find the assignment of shards (keys) to groups,
+// and then talks to the group that holds the key's shard.
+//
+
+import (
+
+	"github.com/shrtyk/raft/kvsrv1/rpc"
+	"github.com/shrtyk/raft/kvtest1"
+	"github.com/shrtyk/raft/shardkv1/shardctrler"
+	"github.com/shrtyk/raft/tester1"
+)
+
+type Clerk struct {
+	clnt *tester.Clnt
+	sck  *shardctrler.ShardCtrler
+	// You will have to modify this struct.
+}
+
+// The tester calls MakeClerk and passes in a shardctrler so that
+// client can call it's Query method
+func MakeClerk(clnt *tester.Clnt, sck *shardctrler.ShardCtrler) kvtest.IKVClerk {
+	ck := &Clerk{
+		clnt: clnt,
+		sck:  sck,
+	}
+	// You'll have to add code here.
+	return ck
+}
+
+
+// Get a key from a shardgrp.  You can use shardcfg.Key2Shard(key) to
+// find the shard responsible for the key and ck.sck.Query() to read
+// the current configuration and lookup the servers in the group
+// responsible for key.  You can make a clerk for that group by
+// calling shardgrp.MakeClerk(ck.clnt, servers).
+func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
+	// You will have to modify this function.
+	return "", 0, ""
+}
+
+// Put a key to a shard group.
+func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
+	// You will have to modify this function.
+	return ""
+}
